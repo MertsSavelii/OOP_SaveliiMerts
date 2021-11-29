c3ca5180cea93ad715953a258bd2789a46a19ff9
MertsSavelii <mokrakrasawa1@gmail.com>
2021-11-30 02:06:04 +0300

    w

---
+++ lab7/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "tbinarytree.h"
+#include "octagon.h"
+
+int main()
+{
+    Octagon octi[8];
+    TBinaryTree<Octagon> tree;
+    for (int i = 0; i < 2; i++)
+    {
+        std::cin>>octi[i];
+        tree.Push(octi[i]);
+    }
+    
+    std::cout << tree << std::endl;
+    //octi[1] = tree.GetItemNotLess(octi[2].Area());
+    //std::cout << octi[1] << std::endl;
+    //std::cout << tree.Count(octi[0]) << std::endl;
+    //tree.Pop(octi[0]);
+    //tree.Pop(octi[0]);
+    //std::cout << tree << std::endl;
+    //tree.Clear();
+    //std::cout << tree << std::endl;
+    //if (tree.Empty())
+        //std::cout << "дерево пустое" << std::endl;
+
+    TBinaryTree<Octagon> q(tree);
+    for (auto i: q)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
---
+++ lab7/octagon.h
@@ -0,0 +1,28 @@
+#ifndef OCTAGON_H
+#define OCTAGON_H
+
+#include <iostream>
+
+#include "figure.h"
+#include "point.h"
+
+class Octagon : public Figure {
+public:
+    Octagon();
+    Octagon(Point a, Point b, Point c, Point d, Point e, Point f, Point g, Point h);
+
+    friend std::istream& operator>>(std::istream& is,  Octagon& obj);
+    friend std::ostream& operator<<(std::ostream& os, const Octagon& obj);
+
+    size_t VertexesNumber();
+    double Area() const;
+    const Octagon& operator=(const Octagon& other);
+    bool operator==(const Octagon& other) const;
+    bool operator!=(const Octagon& other) const;
+
+    ~Octagon();
+private:
+    Point a_, b_, c_, d_, e_, f_, g_, h_;
+};
+
+#endif
